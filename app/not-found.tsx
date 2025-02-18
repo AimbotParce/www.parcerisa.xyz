9dae66a1bcd8b24326c233ef728a02e0060eb9ee
AimbotParce <marcparcerisa@gmail.com>
2025-02-18 15:17:01 +0100

    Not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link"
 
-export default function MissingForwardedPage() {
+export default function MissingPage() {
     return (
         <main className="flex flex-col items-center justify-center gap-8 w-screen h-screen">
             <p>
-                You should have been forwarded to web page, but this might be under construction or not yet available.
+                <span className="font-bold">404</span> - Page not found
             </p>
             <Link href="/" className="hover:underline hover:underline-offset-4 font-bold">
                 ← Back to home
